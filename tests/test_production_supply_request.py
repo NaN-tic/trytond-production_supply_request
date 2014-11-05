75ada086546437f44331db16d3926a49cf122ac9
Sergi Almacellas Abellana <sergi@koolpi.com>
2014-11-05 10:21:11 +0100

    Migrate to version 3.4

--- tests/test_production_supply_request.py
+++ tests/test_production_supply_request.py
@@ -7,17 +7,13 @@ from trytond.tests.test_tryton import test_depends
 
 
 class TestCase(unittest.TestCase):
-    '''
-    Test module.
-    '''
+    'Test module'
 
     def setUp(self):
         trytond.tests.test_tryton.install_module('production_supply_request')
 
     def test0006depends(self):
-        '''
-        Test depends.
-        '''
+        'Test depends'
         test_depends()
 
 
